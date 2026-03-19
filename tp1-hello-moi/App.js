5476f90f960d78f6140cd34740ffd3ac602af79e
Julien MICHELIN <julienmichelin35@gmail.com>
2026-03-19 11:14:42 +0100

    essais sur les styles, la disposition flexbox et les composants view, text et image.

--- tp1-hello-moi/App.js
+++ tp1-hello-moi/App.js
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.titre}>Hello moi !</Text>
+      <Text style={styles.sousTitre}>BTS SIO Lycée VHB</Text>
+      <View style={styles.images}>
+        <Image style={styles.logoAndroid} source={require('./assets/logo-android.jpg')} />
+        <Image style={styles.logoReact} source={require('./assets/tiny-logo.png')}/>
+      </View>
     </View>
   );
 }
@@ -11,13 +16,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f4ff',
+    backgroundColor: '#2E2A28',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titre: {
     fontSize: 36,
     fontWeight: 'bold',
-    color: '#3b5bdb',
+    color: '#DFDAD8',
   },
+  sousTitre: {
+    fontSize: 20,
+    color: '#DFDAD8',
+  },
+  logoAndroid: {
+    width: 75,
+    height: 75,
+    alignSelf: 'flex-start',
+    marginEnd: 15,
+  },
+  logoReact: {
+    width: 75,
+    height: 75,
+    alignSelf: 'flex-end',
+  },
+  images: {
+    flexDirection: 'row',
+  }
 });
