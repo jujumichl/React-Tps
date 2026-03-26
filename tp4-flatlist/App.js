d2e0038df68917344690c0e979cf13ff16614672
Julien MICHELIN <julienmichelin35@gmail.com>
2026-03-26 10:38:30 +0100

    Ajout du séparateur + du clic

--- tp4-flatlist/App.js
+++ tp4-flatlist/App.js
@@ -3,6 +3,12 @@ import { stations } from './data/stations';
 import ItemStation from './components/ItemStation';
 
 export default function App() {
+  function Separateur () {
+    return (
+      <View style={{ height: 1, backgroundColor: '#ddd', marginVertical: 2 }}    	/>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.titre}>Stations de vélos</Text>
@@ -11,6 +17,7 @@ export default function App() {
         data={stations}                         // on passe le tableau d'objets importés depuis stations.js
         keyExtractor={(item) =>                 // React a besoin d'une clé unique par élément pour optimiser les re-rendus. On utilise idstation converti en string.
           item.idstation.toString()}
+        ItemSeparatorComponent={Separateur}
         renderItem={({ item }) => (             // Reçoit un objet avec la propriété item. On déstructure avec ({ item }) pour accéder directement à l'objet station.
           <ItemStation station={item} />
         )}
